3142c53759928252c09ea85fa796f29766fe15af
Kieran Bailey <k.d.bailey88@gmail.com>
2023-02-11 11:33:41 -0600

    Cleaned up some formatting and removed unnessessary data displays

--- scripts/movie_script.sql
+++ scripts/movie_script.sql
@@ -9,20 +9,22 @@ LIMIT 1;
 -- Semi-Tough, 1977, $37,187,139
 
 -- 2. What year has the highest average imdb rating?
-SELECT release_year, AVG(imdb_rating) AS avg_imdb
+SELECT release_year
 FROM specs
 INNER JOIN rating ON specs.movie_id = rating.movie_id
 GROUP BY release_year
-ORDER BY avg_imdb DESC;
+ORDER BY AVG(imdb_rating) DESC
+LIMIT 1;
 -- 1991
 
 -- 3. What is the highest grossing G-rated movie? Which company distributed it?
-SELECT film_title, company_name, worldwide_gross 
+SELECT film_title, company_name 
 FROM specs
 INNER JOIN revenue ON specs.movie_id = revenue.movie_id
 LEFT JOIN distributors ON distributor_id = domestic_distributor_id
 WHERE mpaa_rating = 'G'
-ORDER BY worldwide_gross DESC;
+ORDER BY worldwide_gross DESC
+LIMIT 1;
 -- Toy Story 4, Walt Disney
 
 -- 4. Write a query that returns, for each distributor in the distributors table, the distributor name and the number of movies associated with that distributor in the movies table. Your result set should include all of the distributors, whether or not they have any movies in the movies table.
@@ -47,8 +49,7 @@ FROM specs
 INNER JOIN distributors ON distributor_id = domestic_distributor_id
 INNER JOIN rating ON specs.movie_id = rating.movie_id
 WHERE headquarters NOT LIKE '%CA%'
-ORDER BY imdb_rating DESC
-LIMIT 1;
+ORDER BY imdb_rating DESC;
 -- Dirty Dancing
 
 -- 7. Which have a higher average rating, movies which are over two hours long or movies which are under two hours?
