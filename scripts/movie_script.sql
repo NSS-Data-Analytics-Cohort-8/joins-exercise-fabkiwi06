a2a2282ea064ee38b1e4c0632be9c33452a57601
Kieran Bailey <k.d.bailey88@gmail.com>
2023-02-11 10:36:56 -0600

    Question 5

--- scripts/movie_script.sql
+++ scripts/movie_script.sql
@@ -36,7 +36,12 @@ ORDER BY count_movies DESC;
 -- 5. Write a query that returns the five distributors with the highest average movie budget.
 SELECT company_name
 FROM distributors
-LEFT JOIN specs ON specs ON distributor_id = domestic_distributor_id
+INNER JOIN specs ON distributor_id = domestic_distributor_id
+INNER JOIN revenue ON specs.movie_id = revenue.movie_id
+GROUP BY company_name
+ORDER BY AVG(film_budget) DESC
+LIMIT 5;
+
 
 -- 6. How many movies in the dataset are distributed by a company which is not headquartered in California? Which of these movies has the highest imdb rating?
 
